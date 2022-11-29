031cc3e6dfa7b02bd435882946613f68875f0873
thanhnguyen195 <thanhnguyen.hampshire@gmail.com>
2022-11-29 08:40:07 +0700

    Separate UI Login View with WalletConnect and Glaip custom dependency

---
+++ DreamerlySdk/DreamerlySdk/Glaip/API/Domain/WalletType.swift
@@ -0,0 +1,12 @@
+//
+//  WalletType.swift
+//  
+//
+//  Created by Mauricio Vazquez on 15/8/22.
+//
+
+public enum WalletType: String, Equatable {
+  case MetaMask = "https://metamask.app.link/wc?uri="
+  case Rainbow = "https://rnbwapp.com/wc?uri="
+  case TrustWallet = "https://link.trustwallet.com/wc?uri="
+}
---
+++ DreamerlySdk/DreamerlySdk/Glaip/API/Glaip.swift
@@ -0,0 +1,66 @@
+//
+//  WristBandAPI.swift
+//  
+//
+//  Created by Mauricio Vazquez on 15/8/22.
+//
+
+import SwiftUI
+
+public final class Glaip: ObservableObject {
+  private let walletConnect: WalletLinkService
+
+  public let title: String
+  public let description: String
+  public let supportedWallets: [WalletType]
+
+  public var currentWallet: WalletType?
+
+  @Published public var userState: UserState = .unregistered
+
+  public init(title: String, description: String, supportedWallets: [WalletType]) {
+    self.title = title
+    self.description = description
+    self.supportedWallets = supportedWallets
+    self.walletConnect = WalletLinkService(title: title, description: description)
+  }
+
+  public func loginUser(type: WalletType, completion: @escaping (Result<User, Error>) -> Void) {
+    walletLogin(wallet: type, completion: { [weak self] result in
+      switch result {
+      case let .success(user):
+        DispatchQueue.main.async {
+          self?.userState = .loggedIn(user)
+        }
+        self?.currentWallet = type
+        completion(.success(user))
+      case let .failure(error):
+        completion(.failure(error))
+      }
+    })
+  }
+
+  public func logout() {
+    walletConnect.disconnect()
+  }
+
+  private func walletLogin(wallet: WalletType, completion: @escaping (Result<User, Error>) -> Void) {
+    let service = WalletLinkService(title: title, description: description)
+    service.connect(wallet: wallet, completion: { result in
+
+      switch result {
+      case let .success(walletDetails):
+        completion(.success(
+          User(
+            wallet: Wallet(
+              type: wallet,
+              address: walletDetails.address,
+              chainId: String(walletDetails.chainId))
+          ))
+        )
+      case let .failure(error):
+        completion(.failure(error))
+      }
+    })
+  }
+}
